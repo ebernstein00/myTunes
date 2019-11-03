93f646bd04d6db40c5b65a6846460e301f65f2cc
Marioisdabest <mtutuncumacias1@gmail.com>
2019-11-03 14:15:15 -0500

    added more of the needed functions but can't fix seg fault

--- tunes.c
+++ tunes.c
@@ -10,6 +10,15 @@ struct song_node{
   struct song_node* next;
 };
 
+int getIndex(char firstChar){
+  if(firstChar<97 && firstChar>122){
+    return 0;
+  }
+  else{
+    return firstChar-96;
+  }
+}
+
 struct song_node** add_song(struct song_node* library[27],char artist[100], char music[100]){
   char firstChar = artist[0];
   if(firstChar<97 && firstChar>122){
@@ -26,5 +35,30 @@ void printLibrary(struct song_node* library[27]){
   int i;
   for(i=0;i<27;i++){
     print_list(library[i]);
+  }
+}
+
+struct song_node* search_for_song(struct song_node* library[27], char artist[100], char music[100]){
+  char firstChar = artist[0];
+  int index = getIndex(firstChar);
+  return search(library[index],artist,music);
+}
+
+struct song_node* search_for_artist(struct song_node* library[27], char artist[100]){
+  char firstChar = artist[0];
+  int index = getIndex(firstChar);
+  return search_artist(library[index],artist);
+}
+
+void print_letter(struct song_node* library[27], char letter){
+  int index = getIndex(letter);
+  print_list(library[index]);
+}
+
+void shuffle(struct song_node* library[27], int numSongs){
+  int i;
+  for(i=0;i<numSongs;i++){
+    struct song_node* newSong = random_song(library[rand()%27]);
+    printf("%s by %s", newSong->song, newSong->name);
   }
 }
--- tunes.h
+++ tunes.h
@@ -1,3 +1,13 @@
+int getIndex(char firstChar);
+
 struct song_node** add_song(struct song_node* library[27],char artist[100], char music[100]);
 
 void printLibrary(struct song_node* library[27]);
+
+struct song_node* search_for_song(struct song_node* library[27], char artist[100], char music[100]);
+
+struct song_node* search_for_artist(struct song_node* library[27], char artist[100]);
+
+void print_letter(struct song_node* library[27], char letter);
+
+void shuffle(struct song_node* library[27], int numSongs);
